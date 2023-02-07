0160c75de9cc8996bade7583489fea7f51bfd896
Veronika Moskaliuk <veronika.moskaliuk.ua@gmail.com>
2023-02-07 20:47:22 +0100

    refactor in hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
@@ -8,18 +8,17 @@ import { Filter } from 'components/Filter/Filter';
 import { Container, Title, TitleContacts } from 'components/Container.styles';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(()=> {
-    const contacts = JSON.parse(localStorage.getItem("contacts"));
+  const [contacts, setContacts] = useState(() => {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
     return contacts ? contacts : [];
   });
-  const [filter, setFilter] = useState("");
+  const [filter, setFilter] = useState('');
 
-  useEffect(()=> {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-
-  const addContact = (data) => {
+  const addContact = data => {
     const { name, number } = data;
     if (checkDoubleContact(data)) {
       alert(`${name} is already in your contacts!`);
@@ -33,15 +32,15 @@ export const App = () => {
     setContacts(prevState => [newContact, ...prevState]);
   };
 
-  const checkDoubleContact = (inputData) => {
+  const checkDoubleContact = inputData => {
     return contacts.find(contact => contact.name === inputData.name);
   };
 
-  const deleteContact = (id) => {
+  const deleteContact = id => {
     setContacts(prevState => prevState.filter(el => el.id !== id));
   };
 
-  const handleInputChange = (e) => {
+  const handleInputChange = e => {
     setFilter({
       [e.currentTarget.name]: e.currentTarget.value,
     });
@@ -54,64 +53,47 @@ export const App = () => {
 
   const visibleContacts = getVisibleContacts();
 
-    return (
-      <Container>
-        <Title>Phonebook</Title>
-        <ContactForm onSubmit={addContact} />
-        <TitleContacts>Contacts</TitleContacts>
-        <Filter value={filter} onFilter={handleInputChange} />
-        <ContactList contacts={visibleContacts} deleteContact={deleteContact} />
-      </Container>
-    );
-}
-
+  return (
+    <Container>
+      <Title>Phonebook</Title>
+      <ContactForm onSubmit={addContact} />
+      <TitleContacts>Contacts</TitleContacts>
+      <Filter value={filter} onFilter={handleInputChange} />
+      <ContactList contacts={visibleContacts} deleteContact={deleteContact} />
+    </Container>
+  );
+};
 
 //export class App extends Component {
-  //state = {
-    //contacts: [],
-    //filter: '',
-  //};
+//state = {
+//contacts: [],
+//filter: '',
+//};
 
-  //componentDidMount() {
-    //console.log('App componentDidMount');
+//componentDidMount() {
+//console.log('App componentDidMount');
 
-    //const contacts = localStorage.getItem('contacts');
-    //const parsedContacts = JSON.parse(contacts);
+//const contacts = localStorage.getItem('contacts');
+//const parsedContacts = JSON.parse(contacts);
 
-    //if(parsedContacts){
-     // this.setState({contacts: parsedContacts});
-    //}
-
-    //console.log(parsedContacts);
-    //const contacts = JSON.parse(localStorage.getItem("my-contacts"));
-    //this.setState({contacts})
-  //}
-
-  //componentDidUpdate(prevProps, prevState) {
-    //console.log('App componentDidUpdate');
-
-   // if(this.state.contacts !== prevState.contacts){
-      //console.log('Оновилось поле contacts, записую contacts в сховище');
-
-     // localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-    //}
-    //const {contacts} = this.state;
-    //localStorage.setItem("my-contacts", JSON.stringify(contacts))
-  //}
-
- 
-
-  
-
-  
-
-  
-
-  
-
-  
-    
-    
-  
+//if(parsedContacts){
+// this.setState({contacts: parsedContacts});
 //}
 
+//console.log(parsedContacts);
+//const contacts = JSON.parse(localStorage.getItem("my-contacts"));
+//this.setState({contacts})
+//}
+
+//componentDidUpdate(prevProps, prevState) {
+//console.log('App componentDidUpdate');
+
+// if(this.state.contacts !== prevState.contacts){
+//console.log('Оновилось поле contacts, записую contacts в сховище');
+
+// localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+//}
+//const {contacts} = this.state;
+//localStorage.setItem("my-contacts", JSON.stringify(contacts))
+//}
+//}
